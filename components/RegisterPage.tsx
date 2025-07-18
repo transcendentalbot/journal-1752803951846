bac9f01344042e70b5f11c8955268263b8fbbd35
transcendentalbot <admin@transcendentalbot.net>
2025-07-17 21:59:27 -0400

    Add components/RegisterPage.tsx for Develop: User Registration

---
+++ components/RegisterPage.tsx
@@ -0,0 +1,10 @@
+import RegisterForm from '@/components/RegisterForm'
+
+export default function RegisterPage() {
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-3xl font-bold mb-4">Register</h1>
+      <RegisterForm />
+    </div>
+  )
+}
